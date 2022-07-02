7c254784cc44425b3abcfdb87a641f45c82b4185
wescleytorres <jean.torres@bitcapital.com.br>
2022-07-02 16:33:08 -0300

    feat: started module03, tests componet Search

--- Modulo01/src/lib/Cart.js
+++ Modulo01/src/lib/Cart.js
@@ -1,12 +1,82 @@
-export default class Cart {
+import find from 'lodash/find';
+import remove from 'lodash/remove';
+import Dinero from 'dinero.js'
+
+const calculatePercentageDiscount = (amount, item) => {
+  
+  if (
+    item.condition?.percentage &&
+    item.quantity > item.condition.minimum
+    )
+    {
+      return amount.percentage(item.condition.percentage)
+    }
+
+    return Money({amount: 0});
+}
+
+const calculateQuantityDiscount = (amount, item) => {
+  const isEven = item.quantity % 2 === 0;
+  if (item.condition?.quantity && item.quantity > item.condition.quantity) {
+    return amount.percentage( isEven ? 50 : 40 );
+  }
+  return Money({amount: 0});
+}
+
+const Money = Dinero;
+
+Money.defaultCurrency = 'BRL';
+Money.defaultPrecision = 2;
+
+export default class Cart { 
   items = [];
 
   add(item) {
+    const itemToFind = {product: item.product}
+    if (find(this.items, itemToFind)) {
+      remove(this.items, itemToFind)
+    }
     this.items.push(item);
   }
+
+  remove(product) {
+    remove(this.items, {product})
+  }
+
   getTotal() {
     return this.items.reduce((acc, item) => {
-      return acc + item.quantity * item.product.price;
-    }, 0);
+      const amount = Money({amount: item.quantity * item.product.price})
+      let discount = Money({  amount: 0 });
+
+      if (item.condition?.percentage) {
+        discount = calculatePercentageDiscount(amount, item);
+      } else if (item.condition?.quantity) {
+        discount = calculateQuantityDiscount(amount, item);
+      }
+      
+      return acc.add(amount).subtract(discount);
+
+    }, Money({ amount: 0 }));
+  }
+
+  summary() {
+    const total = this.getTotal().getAmount();
+    const items = this.items;
+
+    return {
+      total,
+      items,
+    }
+  }
+
+  checkout() {
+    const { total, items } = this.summary();
+
+    this.items = [];
+
+    return {
+      total,
+      items,
+    }
   }
 }
---
+++ modulo03/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+};

d4abd66e85d0ff71cc4ac5418c9e6a937e82f514
wescleytorres <wescleydev95@gmail.com>
2022-07-13 22:46:16 -0300

    test: integration tests part 02

--- modulo03/jest.config.js
+++ modulo03/jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   verbose: true,
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/package.json',
+  ],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
+  // collectCoverageFrom: [
+  //   '<rootDir>/src/components/**/*.js',
+  //   '<rootDir>/src/pages/**/*.js',
+  // ],
 };
---
+++ modulo03/src/components/cart-item.unit.spec.js
@@ -0,0 +1,81 @@
+import { fireEvent } from '@testing-library/dom';
+import { screen, render } from '@testing-library/react';
+import CartItem from './cart-item';
+
+const product = {
+  title: 'Relógio bonito',
+  price: '22.00',
+  image: 'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+}
+
+const addToCart = jest.fn();
+
+const renderCartItem = () => {
+  render(<CartItem product={product} addToCart={addToCart}/>);
+};
+
+describe('CartItem', () => {
+  it('should render CartItem', () => { 
+    renderCartItem();
+
+    expect(screen.getByTestId('cart-item')).toBeInTheDocument()
+  });
+
+  it('should display proper content', () => {
+    renderCartItem();
+
+    const image = screen.getByTestId('image');
+
+    expect(screen.getByText(new RegExp(product.title, 'i'))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(product.price, 'i'))).toBeInTheDocument();
+    expect(image).toHaveProperty('src', product.image);
+    expect(image).toHaveProperty('alt', product.title);
+  });
+
+  it('should display 1 as initial quantity', () => {
+    renderCartItem();
+
+    expect(screen.getByTestId('quantity').textContent).toBe('1');
+    
+  })
+
+  it('should increase quantity by 1 when second button is clicked', async () => {
+    renderCartItem();
+
+    const [buttonIncrease,_] = screen.getAllByRole('button');
+
+    await fireEvent.click(buttonIncrease);
+
+    expect(screen.getByTestId('quantity').textContent).toBe('2');
+  })
+
+  it('should increase quantity by 1 when first button is clicked', async () => {
+    renderCartItem();
+
+    const [buttonIncrease, ButtonDecrease] = screen.getAllByRole('button');
+    const quantity = screen.getByTestId('quantity')
+
+    await fireEvent.click(buttonIncrease);
+
+    expect(quantity.textContent).toBe('2');
+
+    await fireEvent.click(ButtonDecrease);
+
+    expect(quantity.textContent).toBe('1');
+  })
+
+  it('should not go below zero in the quantity', async () => {
+    renderCartItem();
+
+    const [_, ButtonDecrease] = screen.getAllByRole('button');
+    const quantity = screen.getByTestId('quantity')
+
+
+    expect(quantity.textContent).toBe('1');
+
+    await fireEvent.click(ButtonDecrease);
+    await fireEvent.click(ButtonDecrease);
+
+    expect(quantity.textContent).toBe('0');
+  })
+});

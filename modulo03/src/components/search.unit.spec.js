1c9e01a6d74a6014679c251481f0a55fbb761cbb
wescleytorres <wescleydev95@gmail.com>
2022-07-15 12:40:29 -0300

    test: new tests module03

--- modulo03/src/components/search.unit.spec.js
+++ modulo03/src/components/search.unit.spec.js
@@ -5,6 +5,10 @@ import userEvent from '@testing-library/user-event';
 const doSearch = jest.fn();
 
 describe("Search", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render a form', () => {
     render(<Search doSearch={doSearch} />);
 
@@ -39,4 +43,17 @@ describe("Search", () => {
 
     expect(doSearch).toHaveBeenCalledWith(inputText);
   });
+
+  it('should call doSearch when search input is cleared', async () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'some text here';
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, inputText);
+    await userEvent.clear(input);
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
+    expect(doSearch).toHaveBeenCalledWith('');
+  })
 });
--- modulo03/src/pages-tests/product-list.integration.spec.js
+++ modulo03/src/pages-tests/product-list.integration.spec.js
@@ -1,6 +1,8 @@
-import { screen, render, waitFor } from '@testing-library/react';
+import { screen, render, waitFor, fireEvent } from '@testing-library/react';
 import ProductList from '../pages';
 import { makeServer } from '../miragejs/server';
+import { Response } from 'miragejs';
+import userEvent from '@testing-library/user-event';
 
 const renderProductList = () => {
   render(<ProductList />);
@@ -33,10 +35,58 @@ describe('ProductList', () => {
     })
 
   });
-  it.todo('should render the no products message');
-  it.todo('should render the Search component');
-  it.todo('should filter the product list when a search is performed');
-  it.todo('should display error message when promise rejects');
+
+  it('should render the no products message', async () => {
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('no-products')).toBeInTheDocument();
+    })
+  });
+
+  it('should display error message when promise rejects', async () => {
+    server.get('products', () => {
+      return new Response(500, {}, '')
+    })
+
+    renderProductList()
+
+    await waitFor(() => {
+      expect(screen.getByTestId('server-error')).toBeInTheDocument()
+      expect(screen.queryByTestId('no-products')).toBeNull()
+      expect(screen.queryAllByTestId('product-card')).toHaveLength(0)
+    })
+  });
+  
+  it('should filter the product list when a search is performed', async () => {
+    const searchTerm = 'Relógio Bonito';
+    
+    server.createList('product', 2);
+
+    server.create('product', {
+      title: searchTerm,
+    });
+
+    renderProductList();
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(3);
+    })
+
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    await userEvent.type(input, searchTerm);
+    await fireEvent.submit(form);
+
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('product-card')).toHaveLength(1);
+    })
+
+  });
+  
   it.todo('should display the tocal quantity of products');
+  
   it.todo('should display product (singular) when there is only 1 product');
 });
